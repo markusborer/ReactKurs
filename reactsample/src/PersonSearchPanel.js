f97f168f3f89ba39fa33f43b91ed27b8252a11ea
Markus.Borer <markus.borer@adesso.ch>
2018-11-02 22:05:04 +0100

    Uebung 8

--- reactsample/src/PersonSearchPanel.js
+++ reactsample/src/PersonSearchPanel.js
@@ -3,8 +3,13 @@ import PersonSearchForm from './PersonSearchForm';
 import PersonSearchResult from './PersonSearchResult';
 import axios from 'axios';
 import ErrorBoundary from './ErrorBoundary';
+import { fromEvent, of } from 'rxjs';
+import { switchMap, debounceTime } from 'rxjs/operators';
+import { Modal } from 'react-materialize';
 
 class PersonSearchPanel extends React.Component {
+
+    eventTarget = new EventTarget();
 
     constructor(props) {
         super(props);
@@ -12,21 +17,56 @@ class PersonSearchPanel extends React.Component {
         this.onChange = this.onChange.bind(this);
     }
 
+    componentDidMount() {
+        fromEvent(this.eventTarget, "OnChange")
+            .pipe(
+                debounceTime(200)
+            )
+            .pipe(
+                switchMap(
+                    event => axios.get('http://localhost:8080/person?name=' + event.detail)
+                        .catch(error => {
+                            console.log(error);
+                            this.setState({
+                                error: 'Fehler bei der Serveranfrage'
+                            })
+                            return of(undefined);
+                        })
+                )
+            )
+            .subscribe(
+                res => {
+                    if (res.data !== undefined) {
+                        console.log(res.data);
+                        this.setState({
+                            persons: res.data,
+                            error: undefined
+                        });
+                    }
+                }
+            );
+    }
+
     onChange(text) {
-        axios.get('http://localhost:8080/person?name=' + text)
-            .then(res => {
-                this.setState({ persons : res.data });
-            }
+        this.eventTarget.dispatchEvent(
+            new CustomEvent('OnChange', { detail: text })
         );
     }
-  
+
     render() {
         return (
             <ErrorBoundary>
                 <div>
                     <h2 className="left-align">Personensuche</h2>
                     <PersonSearchForm onChange={this.onChange} />
-                    <PersonSearchResult persons={this.state.persons} />
+                    {this.state.error === undefined &&
+                        <PersonSearchResult persons={this.state.persons} />
+                    }
+                    {this.state.error !== undefined &&
+                        <Modal header='Error' open='true'>
+                            {this.state.error}
+                        </Modal>
+                    }
                 </div>
             </ErrorBoundary>
         );
